e3d383759d6c53a2218db41741d886eff268297c
Alex Luecke <aluecke07@gmail.com>
2022-09-11 16:22:35 -0700

    fixing blogging views

--- blogging/views.py
+++ blogging/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
 from blogging.models import Post
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 class BlogListView(ListView):
-    model = Post
+    queryset = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
     template_name = 'blogging/list.html'
 
 class BlogDetailView(DetailView):
-    model = Post
+    queryset = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
     template_name = 'blogging/detail.html'
